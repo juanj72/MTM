436aac118924222227ecf83f770cd2d4fd0f3f1a
juan jose <juan.jara@campusucc.edu.co>
2023-05-07 17:18:25 -0500

    agregacion clases de autenticacion para back

--- user/api/router.py
+++ user/api/router.py
@@ -9,5 +9,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('auth/me', UserView.as_view())
+    path('auth/me', UserView.as_view())
 ]
