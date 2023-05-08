e76ab761e6c533a46d9fa41ce5960f88668f539b
juan jose <juan.jara@campusucc.edu.co>
2023-05-07 20:39:03 -0500

    endpoint para paciente

--- muejeres_trabajando_por_el_meta/urls.py
+++ muejeres_trabajando_por_el_meta/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from paciente.api.router import paciente_router
 
 
 
@@ -42,4 +43,6 @@ urlpatterns = [
     path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path(r'api/', include('user.api.router')),
+    path(r'api/',include(paciente_router.urls)),
+    
 ]
